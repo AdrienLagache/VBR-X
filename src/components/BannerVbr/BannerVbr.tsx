aa8e9c7ec0e7bf84808737a97abf75fc0c67a041
Adrien Lagache <lagacheadrien64@gmail.com>
2024-05-28 16:13:13 +0200

    [refactor] complete responsive for home page

--- src/components/BannerVbr/BannerVbr.tsx
+++ src/components/BannerVbr/BannerVbr.tsx
@@ -1,7 +1,11 @@
 import './BannerVbr.scss';
 
 function BannerVbr(): JSX.Element {
-  return <div className="banner-vbr" />;
+  return (
+    <div className="banner-vbr">
+      <img src="/assets/images/banner.webp" alt="la bannière de la team VBR" />
+    </div>
+  );
 }
 
 export default BannerVbr;
